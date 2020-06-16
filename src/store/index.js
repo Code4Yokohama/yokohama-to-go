18293bdd365b1a0652425bbd6a3d623d8a274fe2
tanaka <tanaka@info-lounge.jp>
2020-06-17 08:35:28 +0900

    Deleted getters

--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,6 @@ export default new Vuex.Store({
     displayType: "map",
     keyword: ""
   },
-  getters: {
-    keyword: state => {
-      return state.keyword;
-    }
-  },
   mutations: {
     setDisplayType(state, payload) {
       state.displayType = payload;
